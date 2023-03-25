989315cad2a16639ff477cad06abe75eb3502779
Vithurshini Subramanieam <vsupu26@gmail.com>
2023-03-25 16:34:35 +0530

    Update Videotry.js

--- src/components/Videotry.js
+++ src/components/Videotry.js
@@ -1,16 +1,15 @@
+// import "node_modules/video-react/dist/video-react.css"; 
 import React from 'react';
-import VideoPlayer from 'react-video-js-player';
-import videofile from '../assets/introVideo.mp4';
+import { Player } from 'video-react';
+import vid from '../assets/introVideo.mp4';
+import img from '../assets/selfie.jpeg';
 
-export default function Videotry() {
-    const vid = videofile;
-    const poster = 'https://images.unsplash.com/photo-1570824104453-508955ab713e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=711&q=80'
-
-    
+export default Videotry => {
   return (
-    <div>
-        <VideoPlayer src={vid} posster={poster}/>
-    </div>
-    
-  )
-}
+    <Player
+      playsInline
+       poster={img}
+      src={vid}
+    />
+  );
+};
